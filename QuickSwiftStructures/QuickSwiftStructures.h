18cbeefe36b8073cb68e5914a8a68c7aee8555d3
Lukasz Czerwinski <lukasz@snitch.co>
2015-11-05 15:42:44 -0800

    Initial Commit

---
+++ QuickSwiftStructures/QuickSwiftStructures.h
@@ -0,0 +1,19 @@
+//
+//  QuickSwiftStructures.h
+//  QuickSwiftStructures
+//
+//  Created by Lukasz Czerwinski on 11/5/15.
+//  Copyright © 2015 Lukasz Czerwinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickSwiftStructures.
+FOUNDATION_EXPORT double QuickSwiftStructuresVersionNumber;
+
+//! Project version string for QuickSwiftStructures.
+FOUNDATION_EXPORT const unsigned char QuickSwiftStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickSwiftStructures/PublicHeader.h>
+
+
